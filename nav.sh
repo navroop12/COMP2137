920c8a13bf86e35c3c955226d563fd076077c4ac
navroop12 <137255528+navroop12@users.noreply.github.com>
2023-07-14 16:26:57 -0400

    Update nav.sh

--- nav.sh
+++ nav.sh
@@ -1,13 +1,20 @@
+#!/bin/bash
+# Script to carry out different setup activities
+# Ask the user to launch a few external command-line tools
 echo "Some command external tools to run press enter to continue 1. net-tools"
 read enter
+# Put in the net-tools
 sudo apt install net-tools
 echo "===================================================================="
+# Change hostname to 'autosrv'
 sudo hostnamectl set-hostname autosrv
 echo "Hostnamer changed to autosrv"
 echo "===================================================================="
+# Request the interface name from the user before configuring the IP
 echo "Enter one interface name to change IPs"
 ip link show | awk ' !/link/ {print $2}'
 read intface
+# Add IP settings to the netplan configuration file
 $intface >> /etc/netplan/*.yaml
 dhcp4: no >> /etc/netplan/*.yaml
 addresses: 192.168.16.21/24 >> /etc/netplan/*.yaml
@@ -15,10 +22,12 @@ gateway4: 192.268.16.1 >> /etc/netplan/*.yaml
 search: [home, arpa, localdomain] >> /etc/netplan/*.yaml
 echo "Interface setting changed"
 echo "===================================================================="
+# Setup necessary software
 echo "Installing software enter Y as per instrucuions"
 sudo apt install openssh-server
 sudo apt install apache2
 sudo apt install squid
+# Set up the firewall
 sudo ufw enable
 sudo ufw allow 22
 sudo ufw allow 80
@@ -27,6 +36,7 @@ sudo ufw allow 3128
 echo "Firewall rules updated, current rules are below:"
 sudo ufw status
 echo "===================================================================="
+# Users Add
 echo "Adding user"
 sudo useradd -m -s /bin/bash dennis
 sudo useradd -m -s /bin/bash aubrey
@@ -60,6 +70,7 @@ sudo ssh-keygen -t ed25519 -f /home/tiger/.ssh/id_ed25519 -C "tiger@generic-vm"
 sudo ssh-keygen -t rsa -b 4096 -f /home/yoda/.ssh/id_rsa -C "yoda@generic-vm"
 sudo ssh-keygen -t ed25519 -f /home/yoda/.ssh/id_ed25519 -C "yoda@generic-vm"
 echo "Add the SSH public keys to the authorized_keys"
+# Each user's authorized_keys file should contain their SSH public keys.
 sudo sh -c 'cat /home/dennis/.ssh/id_rsa.pub >> /home/dennis/.ssh/authorized_keys'
 sudo sh -c 'cat /home/dennis/.ssh/id_ed25519.pub >> /home/dennis/.ssh/authorized_keys'
 sudo sh -c 'echo "ssh-ed25519 AAAAC3NzaC11ZDI1NTE5AAAAIG4rT3vTt990x5kndS4HmgTrKBT8SKzhK4rhGKEVGICI student@generic-vm" >> /home/dennis/.ssh/authorized_keys'
